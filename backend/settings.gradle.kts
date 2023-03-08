f272a919b337110edf484b10e950265a55200da3
pploszczyca <piotrus.plo@gmail.com>
2023-03-08 20:28:29 +0100

    Initialized project

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ZBTZ-Backend"
---
+++ backend/src/test/kotlin/com/pk/zbtz/zbtzbackend/ZbtzBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pk.zbtz.zbtzbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZbtzBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
