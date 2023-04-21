33ad92f3c04c81517417777e351b8608286a3a8b
pploszczyca <piotrus.plo@gmail.com>
2023-04-21 17:43:52 +0200

    Added Movie service and factory

--- backend/src/main/kotlin/com/pk/zbtz/zbtzbackend/controllers/movie/MovieController.kt
+++ backend/src/main/kotlin/com/pk/zbtz/zbtzbackend/controllers/movie/MovieController.kt
@@ -1,16 +1,20 @@
 package com.pk.zbtz.zbtzbackend.controllers.movie
 
+import com.pk.zbtz.zbtzbackend.controllers.MovieDatabase
 import com.pk.zbtz.zbtzbackend.controllers.ResponseWithStatistics
+import com.pk.zbtz.zbtzbackend.controllers.movie.service.MovieServiceFactory
 import com.pk.zbtz.zbtzbackend.domain.Movie
-import com.pk.zbtz.zbtzbackend.domain.Statistics
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/movies")
-class MovieController {
+@RequestMapping
+class MovieController(
+    private val movieServiceFactory: MovieServiceFactory
+) {
 
-    @GetMapping
+    @GetMapping("/{movie_database}/movies")
     fun getMovies(
+        @PathVariable(value = "movie_database") movieDatabase: MovieDatabase,
         @RequestParam(value = "sort", required = false) sort: GetMoviesSorting?,
         @RequestParam(value = "sorting_order", required = false) sortingOrder: GetMoviesSortingOrder?,
         @RequestParam(value = "title", required = false) title: String?,
@@ -22,48 +26,47 @@ class MovieController {
         @RequestParam(value = "max_year", required = false) maxYear: Int?,
         @RequestParam(value = "min_rating", required = false) minRating: Float?,
         @RequestParam(value = "max_rating", required = false) maxRating: Float?
-    ): ResponseWithStatistics<List<Movie>> {
-        // TODO: Implement
+    ): ResponseWithStatistics<List<Movie>> =
+        movieServiceFactory
+            .create(movieDatabase)
+            .getAll(
+                sort = sort,
+                sortingOrder = sortingOrder,
+                title = title,
+                platformName = platformName,
+                actorId = actorId,
+                genreId = genreId,
+                directorId = directorId,
+                minYear = minYear,
+                maxYear = maxYear,
+                minRating = minRating,
+                maxRating = maxRating,
+            )
 
-        return ResponseWithStatistics(
-            data = emptyList(),
-            statistics = Statistics()
-        )
-    }
-
-    @GetMapping("/{movie_id}")
+    @GetMapping("/{movie_database}/movies/{movie_id}")
     fun getMovie(
+        @PathVariable(value = "movie_database") movieDatabase: MovieDatabase,
         @PathVariable("movie_id") movieId: Long,
-    ): ResponseWithStatistics<Movie> {
-        // TODO: Implement
+    ): ResponseWithStatistics<Movie> =
+        movieServiceFactory
+            .create(movieDatabase)
+            .get(movieId = movieId)
 
-        return ResponseWithStatistics(
-            data = null,
-            statistics = Statistics()
-        )
-    }
-
-    @PostMapping
+    @PostMapping("/{movie_database}/movies")
     fun addMovie(
+        @PathVariable(value = "movie_database") movieDatabase: MovieDatabase,
         @RequestBody request: AddMovieRequest
-    ): ResponseWithStatistics<Movie> {
-        // TODO: Implement
+    ): ResponseWithStatistics<Movie> =
+        movieServiceFactory
+            .create(movieDatabase)
+            .add(request = request)
 
-        return ResponseWithStatistics(
-            data = null,
-            statistics = Statistics()
-        )
-    }
-
-    @DeleteMapping("/{movie_id}")
+    @DeleteMapping("/{movie_database}/movies/{movie_id}")
     fun deleteMovie(
+        @PathVariable(value = "movie_database") movieDatabase: MovieDatabase,
         @PathVariable("movie_id") movieId: Long,
-    ): ResponseWithStatistics<Any> {
-        // TODO: Implement
-
-        return ResponseWithStatistics(
-            data = null,
-            statistics = Statistics()
-        )
-    }
+    ): ResponseWithStatistics<Unit> =
+        movieServiceFactory
+            .create(movieDatabase)
+            .delete(movieId = movieId)
 }
