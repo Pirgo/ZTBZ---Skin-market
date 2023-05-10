af9ee26a66cda6c3c44f42cdde8ef34bf6c78558
pploszczyca <piotrus.plo@gmail.com>
2023-05-10 20:49:09 +0200

    Implemented get function in MongoPeopleService

--- backend/src/main/kotlin/com/pk/zbtz/zbtzbackend/controllers/people/service/MongoPeopleService.kt
+++ backend/src/main/kotlin/com/pk/zbtz/zbtzbackend/controllers/people/service/MongoPeopleService.kt
@@ -13,10 +13,11 @@ import com.pk.zbtz.zbtzbackend.utils.execution_timer.ExecutionTimer
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class MongoPeopleService(
-    private val humanMongoRepository: HumanMongoRepository,
+    private val repository: HumanMongoRepository,
     private val executionTimer: ExecutionTimer,
     private val mongoMemorySizeProvider: MongoMemorySizeProvider,
 ) : PeopleService {
@@ -28,7 +29,7 @@ class MongoPeopleService(
         val pageRequest = PageRequest.of(offset ?: 0, pageSize ?: 10)
 
         val elapsedTimeResult = executionTimer.measure {
-            humanMongoRepository.findByFirstNameOrSecondName(
+            repository.findByFirstNameOrSecondName(
                 name = nameToSearch.orEmpty(),
                 pageable = pageRequest,
             )
@@ -68,9 +69,54 @@ class MongoPeopleService(
     ): Int? = if (moviesPage.hasNext()) (offset ?: 0) + (pageSize ?: 10) else null
 
 
+    @OptIn(ExperimentalStdlibApi::class)
     override fun get(humanId: String): ResponseWithStatistics<Human> {
-        TODO("Not yet implemented")
+        val elapsedTimeResult = executionTimer.measure {
+            repository
+                .findById(humanId)
+                .getOrNull()
+                ?.toHuman()
+
+        }
+        val human = elapsedTimeResult.blockResult
+
+        return ResponseWithStatistics(
+            data = human,
+            statistics = getStatistics(elapsedTimeResult)
+        )
     }
+
+    private fun HumanMongoModel.toHuman(): Human {
+        return Human(
+            id = this.id ?: "",
+            firstName = this.firstName,
+            secondName = this.secondName,
+            photoUrl = this.photoUrl,
+            birthday = this.birthday,
+            placeOfBirth = this.placeOfBirth,
+            deathDay = this.deathDay,
+            description = this.description,
+            functions = this.functions.toFunctionsValue()
+        )
+    }
+
+    private fun HumanMongoModel.FunctionsValueMongo.toFunctionsValue(): Human.FunctionsValue =
+        Human.FunctionsValue(
+            director = this.director.map { it.toDirector() },
+            actor = this.actor.map { it.toActor() }
+        )
+
+    private fun HumanMongoModel.FunctionsValueMongo.FunctionMongo.DirectorMongo.toDirector(): Human.FunctionsValue.Function.Director =
+        Human.FunctionsValue.Function.Director(
+            filmId = this.filmId,
+            title = this.title
+        )
+
+    private fun HumanMongoModel.FunctionsValueMongo.FunctionMongo.ActorMongo.toActor(): Human.FunctionsValue.Function.Actor =
+        Human.FunctionsValue.Function.Actor(
+            filmId = this.filmId,
+            title = this.title
+        )
 
     override fun add(request: AddHumanRequest): ResponseWithStatistics<Human> {
         TODO("Not yet implemented")
