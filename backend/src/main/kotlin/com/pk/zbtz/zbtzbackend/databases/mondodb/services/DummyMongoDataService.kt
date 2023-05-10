9628de989800ddb03c9d64dff01f0c2919a5d4a2
pploszczyca <piotrus.plo@gmail.com>
2023-05-10 21:56:26 +0200

    Adjusted DummyMongoDataService to generete fake People

--- backend/src/main/kotlin/com/pk/zbtz/zbtzbackend/databases/mondodb/services/DummyMongoDataService.kt
+++ backend/src/main/kotlin/com/pk/zbtz/zbtzbackend/databases/mondodb/services/DummyMongoDataService.kt
@@ -1,24 +1,33 @@
 package com.pk.zbtz.zbtzbackend.databases.mondodb.services
 
+import com.pk.zbtz.zbtzbackend.databases.mondodb.models.HumanMongoModel
 import com.pk.zbtz.zbtzbackend.databases.mondodb.models.MovieMongoModel
+import com.pk.zbtz.zbtzbackend.databases.mondodb.repositories.HumanMongoRepository
 import com.pk.zbtz.zbtzbackend.databases.mondodb.repositories.MovieMongoRepository
 import io.github.serpro69.kfaker.Faker
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import kotlin.math.abs
 
 @Service
 class DummyMongoDataService(
     private val movieRepository: MovieMongoRepository,
+    private val humanMongoRepository: HumanMongoRepository,
 ) {
     @EventListener(ApplicationReadyEvent::class)
     fun generateDummyData() {
         val faker = Faker()
         val numberOfMovies = 10
+        val numberOfHumans = 20
 
         repeat(numberOfMovies) {
             generateFakeMovie(faker).let(movieRepository::save)
+        }
+
+        repeat(numberOfHumans) {
+            generateFakeHuman(faker).let(humanMongoRepository::save)
         }
     }
 
@@ -71,9 +80,45 @@ class DummyMongoDataService(
         )
     }
 
+    private fun generateFakeHuman(faker: Faker): HumanMongoModel {
+        val directorFunctions = (1..3).map {
+            HumanMongoModel.FunctionsValueMongo.FunctionMongo.DirectorMongo(
+                filmId = faker.randomId(),
+                title = faker.movie.title()
+            )
+        }
+
+        val actorFunctions = (1..3).map {
+            HumanMongoModel.FunctionsValueMongo.FunctionMongo.ActorMongo(
+                filmId = faker.randomId(),
+                title = faker.movie.title()
+            )
+        }
+
+        val functionsValue = HumanMongoModel.FunctionsValueMongo(
+            director = directorFunctions,
+            actor = actorFunctions
+        )
+
+        return HumanMongoModel(
+            id = null,
+            firstName = faker.name.firstName(),
+            secondName = faker.name.lastName(),
+            photoUrl = faker.randomUrl(),
+            birthday = faker.birthDate(),
+            placeOfBirth = faker.address.city(),
+            deathDay = faker.birthDate().takeIf { faker.random.nextBoolean() },
+            description = faker.lorem.words(),
+            functions = functionsValue
+        )
+    }
+
     private fun Faker.randomUrl(): String =
         this.string.numerify("www.example####.com")
-    
+
     private fun Faker.randomId(): String =
         this.random.nextLong(Long.MAX_VALUE).let(::abs).toString()
+
+    private fun Faker.birthDate(): LocalDate =
+        this.person.birthDate(age = this.random.nextLong(100))
 }
